e5340313a4278fd156e15bea52ffb44f7f173183
JuanRovito <JuanRovito@172.20.10.2>
2019-09-12 20:40:32 -0300

    Agrego TP1_2 y TP1_3 probado y debuggueado

--- src/TP1.c
+++ src/TP1.c
@@ -54,6 +54,22 @@
 
 /*==================[external functions definition]==========================*/
 
+#define TP1_1 (1)	/* Test & Migrate PROJECT = sapi_examples/edu-ciaa-nxp/bare_metal/gpio/gpio_02_blinky
+					               to PROJECT = projects/SE-2018-TPs/TP1 */
+#define TP1_2 (2)	/* Test & Migrate PROJECT = sapi_examples/edu-ciaa-nxp/bare_metal/gpio/gpio_01_switches_leds
+					               to PROJECT = projects/SE-2018-TPs/TP1 */
+#define TP1_3 (3)	/* Test & Migrate PROJECT = sapi_examples/edu-ciaa-nxp/bare_metal/tick_01_tickHook
+ 	 	 	 	 	               to PROJECT = projects/SE-2018-TPs/TP1 */
+#define TP1_4 (4)	/* Making portable tickHook & LEDs */
+#define TP1_5 (5)	/* Test DEBUG* functions */
+#define TP1_6 (6)	/* Making portable tickHook & LEDs & Push Buttons */
+
+#define TEST (TP1_2)
+
+
+#if (TEST == TP1_1)	/* Test & Migrate PROJECT = sapi_examples/edu-ciaa-nxp/bare_metal/gpio/gpio_02_blinky
+							       to PROJECT = projects/SE-2018-TPs/TP1 */
+
 /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
 int main(void){
 
@@ -80,6 +96,139 @@ int main(void){
    /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
       por ningun S.O. */
    return 0 ;
-}
+
+#endif
+
+
+#if (TEST == TP1_2)	/* Test & Migrate PROJECT = sapi_examples/edu-ciaa-nxp/bare_metal/gpio/gpio_01_switches_leds
+							       to PROJECT = projects/SE-2018-TPs/TP1 */
+   /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+   int main(void)
+   {
+
+      /* ------------- INICIALIZACIONES ------------- */
+
+      /* Inicializar la placa */
+      boardConfig();
+
+      gpioConfig( GPIO0, GPIO_INPUT );
+
+      gpioConfig( GPIO1, GPIO_OUTPUT );
+
+      /* Variable para almacenar el valor de tecla leido */
+      bool_t valor;
+
+      /* ------------- REPETIR POR SIEMPRE ------------- */
+      while(1) {
+
+         valor = !gpioRead( TEC1 );
+         gpioWrite( LEDB, valor );
+
+         valor = !gpioRead( TEC2 );
+         gpioWrite( LED1, valor );
+
+         valor = !gpioRead( TEC3 );
+         gpioWrite( LED2, valor );
+
+         valor = !gpioRead( TEC4 );
+         gpioWrite( LED3, valor );
+
+         valor = !gpioRead( GPIO0 );
+         gpioWrite( GPIO1, valor );
+
+      }
+
+      /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+         por ningun S.O. */
+      return 0 ;
+   }
+
+#endif
+
+
+#if (TEST == TP1_3)	/* Test & Migrate PROJECT = sapi_examples/edu-ciaa-nxp/bare_metal/tick_01_tickHook
+ 	 	 	 	 	               to PROJECT = projects/SE-2018-TPs/TP1 */
+   /* FUNCION que se ejecuta cada vez que ocurre un Tick. */
+   void myTickHook( void *ptr ){
+
+      static bool_t ledState = OFF;
+
+      gpioMap_t led = (gpioMap_t)ptr;
+
+      if( ledState ){
+         ledState = OFF;
+      }
+      else{
+         ledState = ON;
+      }
+      gpioWrite( led, ledState );
+   }
+
+
+   /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+   int main(void){
+
+      /* ------------- INICIALIZACIONES ------------- */
+
+      /* Inicializar la placa */
+      boardConfig();
+
+      /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+         periodicamente una interrupcion cada 50ms que incrementa un contador de
+         Ticks obteniendose una base de tiempos). */
+      tickConfig( 50 );
+
+      /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+         simplemente una funcion que se ejecutara peri�odicamente con cada
+         interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+         a una interrupcion.
+         El segundo parametro es el parametro que recibe la funcion myTickHook
+         al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+      */
+      tickCallbackSet( myTickHook, (void*)LEDR );
+      delay(1000);
+
+      /* ------------- REPETIR POR SIEMPRE ------------- */
+      while(1) {
+         tickCallbackSet( myTickHook, (void*)LEDG );
+         delay(1000);
+         tickCallbackSet( myTickHook, (void*)LEDB );
+         delay(1000);
+         tickCallbackSet( myTickHook, (void*)LED1 );
+         delay(1000);
+         tickCallbackSet( myTickHook, (void*)LED2 );
+         delay(1000);
+         tickCallbackSet( myTickHook, (void*)LED3 );
+         delay(1000);
+         tickCallbackSet( myTickHook, (void*)LEDR );
+         delay(1000);
+      }
+
+      /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+         por ningun S.O. */
+      return 0 ;
+   }
+
+#endif
+
+
+#if (TEST == TP1_4)	/* Making portable tickHook & LEDs */
+
+
+#endif
+
+
+#if (TEST == TP1_5)	/* Test DEBUG* functions */
+
+
+#endif
+
+
+#if (TEST == TP1_6)	/* Making portable tickHook & LEDs & Push Buttons */
+
+
+
+#endif
+
 
 /*==================[end of file]============================================*/
